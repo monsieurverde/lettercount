8e62da2b6668a88187cab327bc67fbb653f83f39
monsieurverde <domverdemgmt@gmail.com>
2019-09-10 23:57:16 -0400

    add code into its own function

--- lettercount.py
+++ lettercount.py
@@ -1,18 +1,30 @@
-x = input("Enter a sentence")
+import string
+def lettercount():
+    
+    x = input("Enter a sentence: ")
 
-x = x.lower()   # convert to all lowercase
+    y = x.lower()   # convert to all lowercase
+    sentence = y.split()
 
-alphabet = 'abcdefghijklmnopqrstuvwxyz'
+    alphabet = 'abcdefghijklmnopqrstuvwxyz'
 
-letter_count = {} # empty dictionary
-for char in x:
-    if char in alphabet: # ignore any punctuation, numbers, etc
-        if char in letter_count:
-            letter_count[char] = letter_count[char] + 1
+    letter_count = {} # empty dictionary
+    for char in sentence:
+        if char in alphabet: # ignore any punctuation, numbers, etc
+            if char in letter_count:
+                letter_count[char] = letter_count[char] + 1
+            else:
+                letter_count[char] = 1
         else:
-            letter_count[char] = 1
+            pass
 
-keys = letter_count.keys()
-keys.sort()
-for char in keys:
-    print(char, letter_count[char])
+    keys = letter_count.keys()
+    keys.sort()
+    print(letter_count)
+
+def main():
+
+    lettercount()
+
+if __name__ == '__main__':
+    main()
